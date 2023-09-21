92e94f369061988d85699a1ebb976d3d6b19a095
kyungkwon01 <kyungkwon01@gmail.com>
2023-09-20 18:57:43 -0600

    fixed inquirer version & edited index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // imports and defining required modules
-
 const inquirer = require('inquirer');
 const fs = require('fs');
+const shapes = require('./lib/shapes');
 
 // user prompt questions using inquirer
 
@@ -12,20 +12,20 @@ function createLogo() {
 				type: 'list',
 				name: 'userShape',
 				message: 'Which shape would you like?',
-				choices: ['circle', 'triangle', 'square'],
+				choices: ['Circle', 'Triangle', 'Square'],
 			},
 			{
 				type: 'list',
 				name: 'userShapeColor',
 				message: 'Which color should the shape be?',
-				choices: ['red', 'blue', 'green', 'black', 'white'],
+				choices: ['Red', 'Blue', 'Green', 'Black', 'White'],
 			},
 			{
 				type: 'input',
 				name: 'userThreeLetters',
 				message: 'Enter three letters that you want in the SVG.',
 				validate: (input) => {
-					if (input === 3) {
+					if (input.length === 3) {
 						return true;
 					}
 					return 'Please enter three letters!';
@@ -35,15 +35,18 @@ function createLogo() {
 				type: 'list',
 				name: 'userLetterColor',
 				message: 'Which color should the text be?',
-				choices: ['red', 'blue', 'green', 'black', 'white'],
+				choices: ['Red', 'Blue', 'Green', 'Black', 'White'],
 			},
 		])
 		// defining svgContent with user's answers from inquirer prompt
 
 		.then((answers) => {
-			const shape = new shapes[answers.userShape](answers.userShapeColor);
+			console.log(answers);
+			const currentShape = new shapes[answers.userShape](
+				answers.userShapeColor
+			);
 			const svgContent = `<svg xmlns="http://www.w3.org/2000/svg" width="300" height="200">
-      ${shape.getSvgCode()}
+      ${currentShape.getSvgCode()}
       <text x="150" y="100" text-anchor="middle" alignment-baseline="middle" font-size="30" fill="${
 			answers.userLetterColor
 		}">${answers.userThreeLetters}</text>
@@ -58,3 +61,5 @@ function createLogo() {
 			});
 		});
 }
+
+createLogo();
